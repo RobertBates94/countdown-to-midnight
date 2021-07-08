8da668c856f90b80599c8c4fa693ae376d0ce1f1
Robert <bates.to.infinity@gmail.com>
2021-07-08 17:32:40 -0500

    commit tree 8fb97358f72f8d90aeb87553199df7f93dc3b37e

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
+require 'pry'
+def countdown(time)
+    time = 10
+    while time >= 1
+        puts "#{time} SECOND(S)!"
+        time -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(time)
+    time = 10
+    while time >= 1
+        puts "#{time} SECOND(S)!"
+        time -= 1
+        sleep(1)
+    end
+    return "HAPPY NEW YEAR!"
 end
